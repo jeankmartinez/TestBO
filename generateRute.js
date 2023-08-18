98150106e27284aff91268671a5fb51d282d81d6
jeankmartinez <jeank432@hotmail.com.ar>
2023-08-18 11:00:23 -0500

    Se agregan comentarios para ser mas legible el codigo y organizacion de un par de condicionales

--- generateRute.js
+++ generateRute.js
@@ -1,13 +1,22 @@
+// Función que genera una ruta basada en datos de entrada
 const generateRoute = (data) => {
-  
+  // Array para almacenar los tamaños de ruta válidos
   const sizes = [];
+  
+  // Divide el grupo de datos en un array de grupos individuales
   const groups = data.groups.split(",");
-
+  
+  // Cantidad de grupos en total
   let cant = groups.length;
+  
+  // Convierte los grupos a números y calcula la suma total
   const numbers = data.groups.split(',').map(Number);
   const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
+  // Ciclo para iterar a través de posibles tamaños de ruta
   for (let x = 1; x <= sum; x++) {
+    
+    //Declaración de variables para validaciones
     let next = true;
     let g = 1;
     let flag = true;
@@ -15,14 +24,24 @@ const generateRoute = (data) => {
     let valid = true;
     let containerValidate = 0;
 
+    // Bucle interno para validar los grupos en la ruta
     while (next && g <= cant) {
+      
+      // Calcula la capacidad restante del contenedor en función de grupos anteriores
       if (flag) {
         containerValidate = x - groups[index];
       } else {
         containerValidate = containerValidate - groups[index];
       }
-      flag = containerValidate > 0 ? false : true;
+      
+      // Establece la bandera según la capacidad del contenedor
+      if (containerValidate > 0) {
+          flag = false;
+      } else {
+          flag = true;
+      }
 
+      // Verifica si la capacidad del contenedor es inválida o si se llegó al final de los grupos
       if (containerValidate < 0 || (containerValidate > 0 && g == cant)) {
         next = false;
         valid = false;
@@ -30,12 +49,15 @@ const generateRoute = (data) => {
       index++;
       g++;
     }
+    // Si la ruta es válida, agrega su tamaño al array de tamaños válidos
     if (valid) {
       sizes.push(x);
     }
   }
 
+  // Retorna los tamaños de ruta válidos como una cadena separada por comas
   return sizes.join(",");
 };
 
+// Exporta la función para su uso externo
 module.exports = generateRoute;
--- index.mjs
+++ index.mjs
@@ -1,27 +1,44 @@
+// Importa la función 'generateRute' desde el archivo 'generateRute.js'
 import generateRute from "./generateRute.js";
 
+// Define una función asincrónica llamada 'handler' que toma un evento como parámetro
 export const handler = async (event) => {
+  // Intenta analizar el cuerpo del evento como JSON y lo asigna a la variable 'data'
   const data = JSON.parse(event?.body);
+
+  // Define un objeto de respuesta inicial con encabezados para JSON
   let response = {
     headers: {
       "Content-Type": "application/json",
     },
   };
 
+  // Obtiene las claves del objeto 'data' y las asigna a la variable 'key'
   const key = Object.keys(data);
-  let error =
-    key != "groups" && `Nombre parametro incorrecto, intente con (groups)`;
 
+  // Inicializa una variable 'error'
+  let error;
+
+  // Verifica si 'key' no es igual a "groups", en cuyo caso establece un mensaje de error
+  if (key != "groups") {
+      error = "Nombre parámetro incorrecto, intente con (groups)";
+  }
+
+  // Si no hay error hasta ahora, se realizan más verificaciones
   if (!error) {
-    const grupos = data.groups.split(",");
+    // Divide el valor de 'groups' en una matriz de grupos individuales
+    //const grupos = data.groups.split(",");
 
+    // Define una expresión regular para validar el formato de los grupos (números positivos separados por comas)
     const regex = /^(?:[1-9]\d*|0)(?:,[1-9]\d*|,0)*$/;
 
+    // Si el formato no cumple con la expresión regular, se establece un mensaje de error
     if (!regex.test(data.groups)) {
       error = "Validar parametros, solo se admiten numeros positivos";
     }
   }
 
+  // Si hay un error, se devuelve una respuesta de error; de lo contrario, se genera el tamaño de ruta y se devuelve una respuesta exitosa
   if (error) {
     return { ...response, statusCode: 500, body: JSON.stringify({ error }) };
   } else {
